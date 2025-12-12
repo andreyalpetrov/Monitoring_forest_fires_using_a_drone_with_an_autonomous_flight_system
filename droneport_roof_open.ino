027df9c4a039d40499fd67c1815fe45e16cf68a5
andreyalpetrov <128998530+andreyalpetrov@users.noreply.github.com>
2025-12-13 00:51:58 +0300

    adapt code for esp 8266; need to change WIFI ssid

--- droneport_roof_open.ino
+++ droneport_roof_open.ino
@@ -1,35 +1,68 @@
-const int r1 = 3;
-const int r2 = 4;
-const int r3 = 5;
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "androprojects";
+const char* password = "4959538539";
+
+ESP8266WebServer server(80);
+
 
 void setup() {
-  pinMode(9, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
+  Serial.begin(115200);
+  
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  resetPins();
 
-  pinMode(r1, OUTPUT);
-  pinMode(r2, OUTPUT);
-  pinMode(r3, OUTPUT);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  
+  Serial.println();
+  Serial.println(WiFi.localIP());
 
-  digitalWrite(r1, LOW);
-  digitalWrite(r2, LOW);
-  digitalWrite(r3, LOW);
+  server.on("/", handleRoot);
+  server.on("/esp8266/roofopen", handleRoofOpen);
+  server.on("/esp8266/roofclose", handleRoofClose);
+  
+  server.begin();
+}
+
+
+void resetPins() {
+  digitalWrite(D0, LOW);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+}
+
+void handleRoot() {
+  String html = "<html><body>";
+  html += "<h1>Roof Control</h1>";
+  html += "<p><a href='/esp8266/roofopen'>Open Roof</a></p>";
+  html += "<p><a href='/esp8266/roofclose'>Close Roof</a></p>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
+}
+
+void handleRoofOpen() {
+  server.send(200, "text/plain", "Opening Roof...");
+  digitalWrite(D0, HIGH);
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  delay(11000);
+  resetPins();
+}
+
+void handleRoofClose() {
+  server.send(200, "text/plain", "Closing Roof...");
+  digitalWrite(D0, HIGH);
+  delay(11000);
+  resetPins();
 }
 
 void loop() {
-  if (digitalRead(9) == LOW) {
-    digitalWrite(r3, HIGH);
-    delay(13000);
-    digitalWrite(r3, LOW);
-  }
-
-  if (digitalRead(12) == LOW) {
-    digitalWrite(r1, HIGH);
-    digitalWrite(r2, HIGH);
-    delay(200);
-    digitalWrite(r3, HIGH);
-    delay(13000);
-    digitalWrite(r1, LOW);
-    digitalWrite(r2, LOW);
-    digitalWrite(r3, LOW);
-  }
+  server.handleClient();
 }
